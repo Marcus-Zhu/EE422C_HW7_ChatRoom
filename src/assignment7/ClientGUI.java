73c2fcf659587a1e6c334645d64a309d1d1746a5
Andrew W <andrwww@utexas.edu>
2016-11-29 21:28:23 -0600

    Added more GUI elements: change password popup, chat modes

--- src/assignment7/ClientGUI.java
+++ src/assignment7/ClientGUI.java
@@ -4,15 +4,19 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+/*import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.collections.FXCollections;
+import javafx.collections.FXCollections;*/
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.scene.Parent;
+//import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -21,15 +25,15 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.stage.Popup;
+//import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.concurrent.Service;
+//import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import javax.swing.event.ChangeEvent;
+//import javax.swing.event.ChangeEvent;
 //import javax.swing.event.ChangeListener;
 
 import assignment7.ChatClient;
@@ -38,6 +42,7 @@ public class ClientGUI extends Application {
 	ObservableList<String> options;
 	ChatClient cc = new ChatClient();
 	TextArea taChatHist;
+	boolean groupChatMode = false;
 	//private StringProperty textRecu = new SimpleStringProperty();
 
 	ArrayList<String> chatHist = new ArrayList<String>();
@@ -95,6 +100,17 @@ public class ClientGUI extends Application {
         ObservableList<String> options = FXCollections.observableArrayList(cc.serverUserList); */
         ComboBox<String> cbMsgRecepient = new ComboBox<String>(options);
         GridPane.setColumnSpan(cbMsgRecepient, 3);
+        Label lbRecipient = new Label("Recipient:");
+        GridPane.setHalignment(lbRecipient, HPos.LEFT);
+
+        //dropdown menu to change mode from individual <-> group chat
+        ArrayList <String> modeOpt = new ArrayList<String>();
+        modeOpt.add("Individual Chat"); modeOpt.add("Group Chat");
+        ComboBox<String> cbModeSel = new ComboBox<String>(FXCollections.observableArrayList(modeOpt));
+        cbModeSel.setValue(modeOpt.get(0));  //initial value
+        GridPane.setColumnSpan(cbModeSel, 3);
+        Label lbMode = new Label("Current Mode:");
+        GridPane.setHalignment(lbMode, HPos.LEFT);
 
         //make send button
         Button btSend = new Button("Send");
@@ -114,11 +130,10 @@ public class ClientGUI extends Application {
         GridPane.setColumnSpan(taChatHist, 4);
         //taChatHist.textProperty().bind(textRecu);
 
-        Label lbRecipient = new Label("Recipient:");
-        GridPane.setHalignment(lbRecipient, HPos.LEFT);
-
-        chatGPane.add(lbRecipient, 0, 0);
-        chatGPane.add(cbMsgRecepient, 1, 0);
+        //chatGPane.add(lbRecipient, 0, 0);
+        //chatGPane.add(cbMsgRecepient, 1, 0);
+        chatGPane.add(lbMode, 0, 0);
+        chatGPane.add(cbModeSel, 1, 0);
         chatGPane.add(taChatHist, 0, 1);
         chatGPane.add(lbRecip, 0, 2);
         chatGPane.add(tfRecip, 1, 2);
@@ -136,10 +151,38 @@ public class ClientGUI extends Application {
             	cc.message(name, msg);*/
 
             	//method 2
-            	String name =tfRecip.getText();
-            	String msg = tfMsg.getText();
-            	cc.message(name, msg);
+            	if (!groupChatMode){
+            		String name =tfRecip.getText();
+                	String msg = tfMsg.getText();
+                	cc.message(name, msg);
+            	}
+            	else{
+            		String name =tfRecip.getText();
+                	String msg = tfMsg.getText();
+                	//cc.groupMessage(name, msg);
+            	}
+
             }
+        });
+
+        // is group chat selected?
+        cbModeSel.valueProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue ov, String t, String t1) {
+              if (cbModeSel.getValue().equals("Group Chat")){
+            	  groupChatMode = true;
+            	  btSend.setText("Send group");
+            	  System.out.println("Mode: group chat");
+              }
+
+              else{
+            	  groupChatMode = false;
+            	  btSend.setText("Send");
+            	  System.out.println("Mode: individual");
+              }
+
+            }
+
         });
 
 /*        //make the text area autoscroll down
@@ -150,6 +193,36 @@ public class ClientGUI extends Application {
                 //use Double.MIN_VALUE to scroll to the top
             }
         });*/
+
+        // =========================POPUP WINDOW============================
+        GridPane passGPane = new GridPane();
+        Scene passScn = new Scene(passGPane, 320, 180);
+        passGPane.setPadding(new Insets(20, 10, 20, 30));
+        passGPane.setHgap(5); chatGPane.setVgap(5);
+
+        //make labels and text fields
+        //label, text field for old password
+        Label lbOPass = new Label("Old Password:");
+        GridPane.setHalignment(lbOPass, HPos.LEFT);
+        GridPane.setColumnSpan(lbOPass,1);
+        TextField tfOPass = new TextField();
+        GridPane.setColumnSpan(tfOPass, 4);
+
+        //label, text field for new password
+        Label lbNPass = new Label("New Password:");
+        GridPane.setHalignment(lbNPass, HPos.LEFT);
+        GridPane.setColumnSpan(lbNPass,1);
+        TextField tfNPass = new TextField();
+        GridPane.setColumnSpan(tfNPass, 4);
+
+        //make button
+        Button btnConfirm = new Button("Confirm");
+
+        passGPane.add(lbOPass, 0,0);
+        passGPane.add(tfOPass,1,0);
+        passGPane.add(lbNPass, 0,1);
+        passGPane.add(tfNPass,1,1);
+        passGPane.add(btnConfirm,2,2);
 
         // ============create login scene - displays at start===============
         GridPane loginGPane = new GridPane();
@@ -204,6 +277,7 @@ public class ClientGUI extends Application {
             public void handle(ActionEvent event) {
                 System.out.println("Pressed login");
                 cc.login(tfUser.getText(), tfPass.getText());
+                primaryStage.setTitle("Chat client - " + tfUser.getText());
                /* cc.getUserList();
                 while(!cc.userListUpdated);		//TODO: UNCOMMENT!!
                 options = FXCollections.observableArrayList(cc.serverUserList);*/
@@ -240,6 +314,19 @@ public class ClientGUI extends Application {
 
             }
         });
+
+        btChngPass.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Pressed change pass");
+                Stage stgChangePass = new Stage();
+                stgChangePass.setTitle("Change Password");
+                stgChangePass.setScene(passScn);
+                stgChangePass.show();
+
+            }
+        });
+
 
 
 
